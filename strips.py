856539aa847251556fb54c67f14b02fc197529a2
Federico Ferri <federico.ferri.it@gmail.com>
2014-01-20 05:00:47 +0100

    remove unused Variable class

--- strips.py
+++ strips.py
@@ -129,24 +129,3 @@ class State(object):
 
 class UnsatisfiedPreconditions(Exception):
     pass
-
-class Variable(object):
-    auto_id = 1
-
-    def __init__(self, name=None):
-        if name is None:
-            name = '?_x%d' % auto_id
-            auto_id += 1
-        self.name = name
-
-    def __str__(self): return self.name
-
-    def __repr__(self): return self.__str__()
-
-    def __eq__(self, other):
-        return isinstance(other, self.__class__) and self.name == other.name
-
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
-    def __hash__(self): return hash(self.name)
