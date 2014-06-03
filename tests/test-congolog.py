2e9d6e0f36b583b08421ba9e5ed9da0545ed8a2c
Federico Ferri <federico.ferri.it@gmail.com>
2014-06-03 21:23:45 +0200

    blah

--- tests/test-congolog.py
+++ tests/test-congolog.py
@@ -4,20 +4,14 @@ from collections import defaultdict
 from copy import copy
 from strips import *
 from golog_program import *
-from domains.math import S as S1
+from domains.math2 import S
 
 cond = lambda s: s.n < 8
 s = S(3)
-p = PConc(
-    Sequence(
-        Star(Exec(S.double())),
-        Test(cond)
-    ),
-    Sequence(
-        Star(Exec(S.incr())),
-        Test(cond)
-    )
-)
+p = PConc(Star(Exec(S.incr())), Star(Exec(S.double())))
+#p = PConc(Star(Exec(S.double())), Star(Exec(S.incr())))
+#p = Star(Choose(Exec(S.incr()), Exec(S.double())))
+#p = Star(Choose(Exec(S.double()), Exec(S.incr())))
 
 maxsol=10
 for pn, sn, an in trans_star(p, s, []):
