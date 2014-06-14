fb6a5cf0e3916557738ccd088e3634084ba8156c
Federico Ferri <federico.ferri.it@gmail.com>
2014-06-14 16:14:42 +0200

    replace defaultdict with dict() and set() where appropriate; makes it more understandable

--- tests/domains/blocks.py
+++ tests/domains/blocks.py
@@ -1,26 +1,24 @@
 #!/usr/bin/env python3
 
-from collections import defaultdict
-from copy import copy
 from strips import *
 
 class Block(Object):
     pass
 
 class BlocksState(State):
-    def __init__(self):
-        self.on = defaultdict(bool)
-        self.clear = defaultdict(bool)
+    def __init__(self, on=dict(), clear=set()):
+        self.on = on
+        self.clear = clear
 
     @Action
     def move(self, obj: Block, objfrom: Object, objto: Object):
         if objfrom == objto or obj == objto:
             raise UnsatisfiedPreconditions()
-        if not self.on[obj] == objfrom or not self.clear[obj] or not (self.clear[objto] or objto == table):
+        if not self.on[obj] == objfrom or obj not in self.clear or not (objto in self.clear or objto == table):
             raise UnsatisfiedPreconditions()
         self.on[obj] = objto
-        self.clear[objfrom] = True
-        del self.clear[objto]
+        self.clear.add(objfrom)
+        self.clear.discard(objto)
 
 a = Block('a')
 b = Block('b')
@@ -28,13 +26,7 @@ c = Block('c')
 d = Block('d')
 table = Object('table')
 
-s = BlocksState()
-s.clear[b] = True
-s.on[b] = d
-s.on[d] = a
-s.on[a] = table
-s.clear[c] = True
-s.on[c] = table
+s = BlocksState(on = {b: d, d: a, a: table, c: table}, clear = {b, c})
 
-goal = lambda s: s.clear[a] and s.on[a] == b and s.on[b] == c and s.on[c] == d and s.on[d] == table
+goal = lambda s: a in s.clear and s.on[a] == b and s.on[b] == c and s.on[c] == d and s.on[d] == table
 
